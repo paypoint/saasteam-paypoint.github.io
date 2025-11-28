4f106210d950d1f4cb9fe0e76dd10a3f4a14b2b8
Chetan Kantharia <chetan.kantharia@gmail.com>
2025-11-29 03:59:36 +0530

    NOOP: 3

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,10 +20,10 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: "https://saasteam-paypoint.github.io",
+  url: "https://paypoint.github.io/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/saasteam-paypoint.github.io/",
+  baseUrl: "/saasteam/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
